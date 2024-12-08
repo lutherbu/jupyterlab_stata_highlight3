e5251723114934deb119f895ebb2cd3480284d92
lutherbu <luofan.bu@gmail.com>
2024-12-08 11:44:15 +0800

    Apply Copier Template

--- jupyterlab_stata_highlight3/__init__.py
+++ jupyterlab_stata_highlight3/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": "jupyterlab_stata_highlight3"
+        "dest": "jupyterlab-stata-highlight3"
     }]
